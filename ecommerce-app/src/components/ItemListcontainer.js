fd3b094916b9ef7f0327454962ab65bb4a8ac613
Sod4H <manuelpazlorenzo@gmail.com>
2023-11-23 01:42:46 -0300

    corrijo errores

--- ecommerce-app/src/components/ItemListcontainer.js
+++ ecommerce-app/src/components/ItemListcontainer.js
@@ -1,19 +1,9 @@
 import React from 'react';
 
 const ItemListContainer = ({ greeting }) => {
-  const categories = ['Electrónicos', 'Ropa', 'Libros', 'Hogar'];
-
   return (
-    <div className="container mt-4">
-      <h2>{greeting}</h2>
-
-      <div className="list-group">
-        {categories.map((category, index) => (
-          <a key={index} href="#" className="list-group-item list-group-item-action">
-            {category}
-          </a>
-        ))}
-      </div>
+    <div className="item-list-container">
+      <p>{greeting}</p>
     </div>
   );
 };
