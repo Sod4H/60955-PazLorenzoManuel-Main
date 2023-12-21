764c47a180beafc6287e1f321b85d8eede76affc
Sod4H <manuelpazlorenzo@gmail.com>
2023-12-21 19:43:18 -0300

    agrego fotos

--- ecommerce-app/src/components/productList.jsx
+++ ecommerce-app/src/components/productList.jsx
@@ -4,11 +4,11 @@ import './ProductList.css';
 const products = [
   { id: 1, name: 'Campera 1', description: 'Campera naranja', price: 50, imageUrl:'img/Campera1.jpg' },
   { id: 2, name: 'Campera 2', description: 'Campera verde', price: 60, imageUrl:'img/Campera2.jpg' },
-  { id: 3, name: 'Mochila 1', description: 'Descripción de1', price: 110, imageUrl:'' },
-  { id: 4, name: 'Mochila 2', description: 'Descripción del casco 2', price: 100, imageUrl:'' },
-  { id: 5, name: 'Pantalón 1', description: 'Descripción del pantalón 1', price: 90, imageUrl:'' },
-  { id: 6, name: 'Pantalón 2', description: 'Descripción del pantalón 2', price: 85, imageUrl:'' },
-  { id: 7, name: 'Pantalón 3', description: 'Descripción del pantalón 3', price: 80, imageUrl:'' },
+  { id: 3, name: 'Mochila 1', description: 'Mochila azul gris y negra', price: 110, imageUrl:'img/mochila1.jpg' },
+  { id: 4, name: 'Mochila 2', description: 'Mochila azul y celeste', price: 100, imageUrl:'img/mochila2.jpg' },
+  { id: 5, name: 'Pantalón 1', description: 'Pantalón verde', price: 90, imageUrl:'img/pantalon1.jpg' },
+  { id: 6, name: 'Pantalón 2', description: 'Pantalón mostaza', price: 85, imageUrl:'img/pantalon2.jpg' },
+  { id: 7, name: 'Pantalón 3', description: 'Pantalón negro', price: 80, imageUrl:'img/pantalon3.jpg' },
 
 ];
 
